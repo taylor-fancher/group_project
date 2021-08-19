af05350e115939ae54a4f2c29dccd0ab29244325
Matthew Le <matthewthaile@gmail.com>
2021-08-18 17:47:26 -0700

    made adjustments to urls/views.py

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login_user', views.login_user),
     path('register', views.register_page),
     path('register_user', views.register_user),
+    path('logout', views.logout),
     path('trip', views.home),
     path('/trip/new', views.new_trip),
     path('/trip/create', views.create_trip),
@@ -13,8 +14,9 @@ urlpatterns = [
     path('/trip/<int:trip_id>', views.trip_info),
     path('/trip/<int:trip_id>/edit', views.edit_trip),
     path('/trip/<int:trip_id>/delete', views.delete_trip),
-    path('/user/<int:user_id>', views.user_profile),
-    path('/user/<int:user_id>/update', views.update_user),
-    path('/user/<int:user_id>/delete', views.delete_user),
+    path('/trip/<int:user_id>', views.user_profile),
+    path('/user/<int:user_id>/edit', views.edit_your_profile),
+    path('/user/<int:user_id>/update', views.update_your_profile),
+    path('/user/<int:user_id>/delete', views.delete_your_profile),
 
 ]
