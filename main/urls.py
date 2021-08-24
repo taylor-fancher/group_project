21769206e8293a04343c8ee4920502970eab1503
Taylor Fancher <tfanch24@gmail.com>
2021-08-24 15:01:23 -0500

    testing method to add images

--- main/urls.py
+++ main/urls.py
@@ -27,3 +27,5 @@ urlpatterns = [
     path('trip/<int:trip_id>/upload', views.trip_upload),
     path('trip/<int:trip_id>/image', views.add_trip_photo)
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
