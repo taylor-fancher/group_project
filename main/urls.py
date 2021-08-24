9b419547e32260eb26f6662d969a283e7b46670c
Taylor Fancher <tfanch24@gmail.com>
2021-08-24 11:06:36 -0500

    clearing previous work to upload images to start fresh

--- main/urls.py
+++ main/urls.py
@@ -27,5 +27,3 @@ urlpatterns = [
     path('trip/<int:trip_id>/upload', views.trip_upload),
     path('trip/<int:trip_id>/image', views.add_trip_photo)
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
