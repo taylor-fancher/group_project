bfd78ef9a4000ca6a715f6a79bfc64ed7382cc96
Taylor Fancher <tfanch24@gmail.com>
2021-08-25 12:44:15 -0500

    removing image upload feature

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.login_page),
@@ -27,5 +25,3 @@ urlpatterns = [
     path('trip/<int:trip_id>/upload', views.trip_upload),
     path('trip/<int:trip_id>/image', views.add_trip_photo)
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
